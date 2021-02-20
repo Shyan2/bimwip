4ba2c7215a91ea04edf5a5fa4dd8d297b2078ab3
Shyan Chien <chien.shyan@wsptaiwan.com>
2021-02-20 16:20:50 +0800

    newer

---
+++ index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 8080;
+const config = require('./config');
+
+if (
+  config.credentials.client_id === null ||
+  config.credentials.client_secret === null
+) {
+  console.error('Missing credentials.');
+  return;
+}
+
+const app = express();
+app.use(cors());
+
+app.use('/api/forge/oauth', require('./routes/forge/oauth'));
+// app.use('/api/forge/oss', require('./routes/forge/oss'));
+// app.use('/api/forge/modelderivative', require('./routes/forge/model-derivate'));
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode).json(err);
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ routes/forge/oauth.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const axios = require('axios');
+
+const config = require('../../config');
+
+let router = express.Router();
+
+const {
+  getPublicTokenTwoLegged,
+  getInternalTokenTwoLegged,
+} = require('./common/oauth');
+
+// GET /api/forge/oauth/token - generates a public access token for 2-legged-authorization.
+router.get('/token', async (req, res, next) => {
+  try {
+    const accessToken = await getPublicTokenTwoLegged();
+    res.json(accessToken);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/callback/oauth', async (req, res, next) => {
+  const { code } = req.query;
+  const oauth = new OAuth(req.session);
+  try {
+    await oauth.setCode(code);
+    // res.redirect('/');
+    res.redirect('http://localhost:3000/bim360');
+    // res.redirect('https://wsp-internal-forge.herokuapp.com/bim360');
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/oauth/url', (req, res) => {
+  const url =
+    'https://developer.api.autodesk.com' +
+    '/authentication/v1/authorize?response_type=code' +
+    '&client_id=' +
+    config.credentials.client_id +
+    '&redirect_uri=' +
+    config.credentials.callback_url +
+    '&scope=' +
+    config.scopes.internal.join(' ');
+  res.end(url);
+});
+
+router.get('/oauth/signout', (req, res) => {
+  req.session = null;
+  res.redirect('http://localhost:3000/bim360');
+  // res.redirect('https://wsp-internal-forge.herokuapp.com/bim360');
+});
+
+module.exports = router;
